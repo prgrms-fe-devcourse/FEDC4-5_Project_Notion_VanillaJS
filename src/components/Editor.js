8124ac5fcc00ca3c6d3c19e4dab73d624524d205
Cheshier <chungup5495@gmail.com>
2023-06-29 18:54:58 +0900

    Feat: 간단한 에디터 뷰 추가

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,11 @@
-import { Component } from "../core/Component";
+import Component from "../core/Component";
 
 export default class Editor extends Component {
   render() {
     this.el.innerHTML = `
         <div class="container">
-          <p>Editor View</p>
+          <input class="titleInput"/>
+          <textarea class="contentInput" placeholder="Input Something..."></textarea>
         </div>
         `;
     this.el.setAttribute("id", "editor-app");
