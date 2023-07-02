92c92aa0385e645729a7aea134a39d9be59457e9
김민재 <kimminje7267@naver.com>
2023-07-02 13:31:53 +0900

    feat: SideBar 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import SideBar from './components/SideBar.js';
+
 export default class App {
   constructor({ $target }) {
     this.$target = $target;
+    this.$sideBar = new SideBar({
+      $target, initialState: []
+    })
   }
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,41 @@
+export default class SideBar {
+  constructor({ $target, initialState }) {
+    this.state = initialState;
+
+    this.$sideBar = document.createElement('nav');
+    this.$sideBar.className = 'side-bar';
+    $target.appendChild(this.$sideBar);
+
+    this.$postList = document.createElement('ul');
+    this.$postList.className = 'post-list';
+    this.$sideBar.appendChild(this.$postList);
+
+    this.$postList.addEventListener('click', this.handleDocumentClick.bind(this))
+
+    this.makeList();
+  }
+
+  makeList() {
+    this.$postList.innerHTML = this.render(this.state);
+  }
+
+  render(documents) {
+    return documents.map((document) => `
+      <li data-id=${document.id} class="document ${document.documents.length > 0 ? 'has-children' : ''}">
+        ${document.title}
+        ${document.documents.length > 0 ? `<ul class="sub-list">${this.render(document.documents)}</ul>` : ''}
+      </li>
+    `).join('');
+  }
+
+  handleDocumentClick(event) {
+    const { target } = event;
+    const documentElement = target.closest('.document');
+    if (documentElement.classList.contains('has-children')) {
+      const childList = documentElement.querySelector('.sub-list');
+      if (childList) {
+        childList.classList.toggle('visible');
+      }
+    }
+  }
+}
