e080f751ed7da4d6e72cddb84ba7d0e45220616d
박민우 <alsd20177@gmail.com>
2023-07-03 18:57:59 +0900

    Feature: document 관련 API 함수 유틸화

--- src/utils/api.js
+++ src/utils/api.js
@@ -20,11 +20,46 @@ export const request = async (url, options = {}) => {
   }
 };
 
-// 사용법
-// const createdPost = await request("/posts", {
-//   // 새로 post를 생성하는 API => postId 값을 return 해줌.
-//   method: "POST",
-//   body: JSON.stringify(post),
-// });
+// 모든 document 조회
+export const getAllDocumentAPI = async () => {
+  const documentList = await request("");
+  return documentList;
+};
 
-// const posts = await request("/posts"); // 서버로부터 posts 불러오고
+// 특정 document 조회
+export const getDocumentAPI = async (documentId) => {
+  const document = await request(`/${documentId}`);
+  return document;
+};
+
+// 특정 document 생성
+export const createDocumentAPI = async (documentId = null) => {
+  const createdDocument = 
+  await request("", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "new Document",
+      parent: documentId,
+    }),
+  });
+  return createdDocument;
+};
+
+// 특정 document 삭제
+export const deleteDocumentAPI = async (documentId) => {
+  await request(`/${documentId}`, {
+    method: "DELETE",
+  });
+};
+
+// 특정 document 수정
+export const modifyDocumentAPI = async (documentId, newTitle, newContent) => {
+  await request(`/${documentId}`, {
+    // 뭔가 클로져 때문에 위험할 지도? this.state.selectedDocumentId를 전달하는게 낫지 않을까
+    method: "PUT",
+    body: JSON.stringify({
+      title: newTitle,
+      content: newContent,
+    }),
+  });
+};
