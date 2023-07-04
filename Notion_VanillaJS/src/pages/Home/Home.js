90fb00d94c9dc073ad633177b925c2f5ba4b8bb5
Dongja <jdh981118@naver.com>
2023-07-04 16:50:46 +0900

    :lipstick: Design: home page ui  기초 구현

--- Notion_VanillaJS/src/pages/Home/Home.js
+++ Notion_VanillaJS/src/pages/Home/Home.js
@@ -1,7 +1,19 @@
 import { Component } from '@/core';
+import styles from './Home.module.css';
 
 export default class Home extends Component {
   templates() {
-    return `<h1>home</h1>`;
+    return `
+    <section class= '${styles.content}'>
+      <h1 class='${styles.header}'>
+        <span class=${styles.purple}>D</span>ong
+        <span class=${styles.purple}>J</span>a's
+        <span class=${styles.purple}>N</span>otion에 오신 걸 환영합니다!</h1>
+      <p class=${styles.description}>게시글을 추가, 삭제, 수정할 수 있으며<br/> 작성중인 게시글은 자동으로 저장됩니다</p>
+    </section>
+    <footer class = '${styles.footer}'>
+      <p>© 2023. Dongja rights reserved.</p>
+    </footer>
+    `;
   }
 }
