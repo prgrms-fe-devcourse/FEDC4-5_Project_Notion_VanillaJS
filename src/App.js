3ae4ee6df839ba5b527e0f546f19587b5a11d4d9
이민희 <mhl98@dgu.ac.kr>
2023-07-05 03:02:55 +0900

    fix: 주소 이동 시 사이드바 리렌더링

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App({ $target }) {
   const sideBar = new SideBar({ $target });
 
   this.route = () => {
-    // $target.innerHTML = "";
+    $target.innerHTML = ""; // TODO: 전체를 없애지말고 편집기만 비우기 -> sideBar state 업데이트 언제?
     const { pathname } = window.location;
 
     if (pathname === "/") {
@@ -13,6 +13,8 @@ export default function App({ $target }) {
     } else if (pathname.indexOf("/documents/") === 0) {
       const [, , documentId] = pathname.split("/");
       // TODO: 우측 편집기 초기화
+
+      sideBar.setState();
     }
   };
 
