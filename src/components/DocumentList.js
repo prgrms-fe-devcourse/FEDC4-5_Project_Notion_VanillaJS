d05ec512fe425b15f8c8db9328133bc65cea7d0f
judahhh <jdh99080516@gmail.com>
2023-07-06 02:04:08 +0900

    feat : 각 글 목록 보여주기

---
+++ src/components/DocumentList.js
@@ -0,0 +1,54 @@
+import { displayDocumentList } from "../utils/displayDocumentList.js";
+import { push } from "../utils/router.js";
+import { setItem, getItem, removeItem } from "../utils/storage.js";
+
+export default function PostList({ $target, initialState }) {
+  const $documentList = document.createElement("div");
+  $documentList.className = "document-list";
+  $target.appendChild($documentList);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $documentList.innerHTML = `
+            <button name ="add-btn" data-id = "null" class="root-page-add" >페이지 추가</button>
+            <div class ="list-box">
+            ${displayDocumentList(this.state)}
+            </div>
+        `;
+  };
+
+  this.render();
+
+  $documentList.addEventListener("click", (e) => {
+    const { target } = e;
+    const element = target.closest("[name]");
+
+    if (element) {
+      const id = element.dataset.id;
+      const listToggleState = `isOpened-${id}`;
+      const name = element.getAttribute("name");
+      if (target.className === "toggle-btn") {
+        const getToggleState = getItem(listToggleState);
+        getToggleState
+          ? removeItem(listToggleState)
+          : setItem(listToggleState, "block");
+        this.render();
+
+        return;
+      }
+
+      if (name) {
+        push({
+          type: name,
+          id,
+        });
+      }
+    }
+  });
+}
