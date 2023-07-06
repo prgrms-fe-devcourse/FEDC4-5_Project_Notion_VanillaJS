ef56aeae35046ec8fe214353bb36251d5f985fb0
hyesung99 <4538asd@gmail.com>
2023-07-06 15:57:55 +0900

    refator : textarea autofocus 해제

--- src/Component/Editor.component.js
+++ src/Component/Editor.component.js
@@ -4,10 +4,18 @@ export default class EditorComponent extends Component {
   render() {
     this.$target.innerHTML = `
     <div class="editorContainer">
-      <div class="title" contentEditable="true" name="title">
+      <div class="title" ${
+        this.state.id === -1
+          ? `contentEditable="false"`
+          : `contentEditable="true"`
+      } name="title">
         ${this.state.title || "제목을 입력하세요"}
       </div>
-      <div class="textarea" contentEditable="true" name="textarea" autofocus>
+      <div class="textarea" ${
+        this.state.id === -1
+          ? `contentEditable="false"`
+          : `contentEditable="true"`
+      } name="textarea">
         ${this.state.content || ""}
       </div>
     </div>
