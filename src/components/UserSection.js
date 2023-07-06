ed12d43cfd4f648f7ce3014199b89b5ee9f77608
seeyoujeong <chan3358@naver.com>
2023-07-06 17:09:13 +0900

    feat: component에 DocumentSubList, UserSection 추가

---
+++ src/components/UserSection.js
@@ -0,0 +1,22 @@
+import { API_HEADER_X_USERNAME } from "../constants.js";
+import Component from "./Component.js";
+
+export default class UserSection extends Component {
+  render() {
+    this.$target.innerHTML = `
+      <span class="user">📓${API_HEADER_X_USERNAME}</span>
+    `;
+
+    const $div = document.createElement("div");
+    $div.className = "fill";
+    this.$parent.append($div);
+  }
+
+  addEvent() {
+    this.$target.addEventListener("click", (e) => {
+      if (e.target.className === "user") {
+        this.props.onClickUserSection();
+      }
+    });
+  }
+}
