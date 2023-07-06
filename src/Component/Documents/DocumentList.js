88bb8c7ae2cac539375b0cb99b7bf5cd4f6053bd
Kim0426 <706shin1728@naver.com>
2023-07-06 20:14:24 +0900

    feat:편집, 문서 리스트 관련 컴포넌트 추가

---
+++ src/Component/Documents/DocumentList.js
@@ -0,0 +1,60 @@
+import { generateNestedDocumentsHTML } from '../../utils/Document/extractDocument.js';
+// 전체 문서 목록을 보여주는 컴포넌트
+// 각각의 이벤트는 와부애서 주입하게 설정
+import DocumentItemTemplate from '../Template/DocumentItemTemplate.js';
+
+export default function DocumentList({
+  $target,
+  initialState,
+  onDocumentClick,
+  onDocumentAdd,
+  onDocumentDelete,
+}) {
+  const $documentLists = document.createElement('div');
+  $documentLists.className = 'documentList';
+  $target.appendChild($documentLists);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    $documentLists.innerHTML = `
+          ${this.state
+            .map(({ id, title, documents }) => {
+              let html = `<ul">`;
+              html += DocumentItemTemplate({
+                documentId: id,
+                documentTitle: title,
+              });
+              html += generateNestedDocumentsHTML(documents);
+              html += `</ul>`;
+              return html;
+            })
+            .join('')}
+      `;
+  };
+
+  $documentLists.addEventListener('click', (e) => {
+    const $documentItem = e.target.closest('.document');
+    if ($documentItem) {
+      const { id } = $documentItem.dataset;
+      onDocumentClick(id);
+    }
+
+    const $documentDeleteBtn = e.target.closest('.documentDeleteBtn');
+    if ($documentDeleteBtn) {
+      const { id } = $documentDeleteBtn.dataset;
+      onDocumentDelete(id);
+    }
+
+    const $documentAddBtn = e.target.closest('.documentAddBtn');
+    if ($documentAddBtn) {
+      const { id } = $documentAddBtn.dataset;
+      onDocumentAdd(id);
+    }
+  });
+}
