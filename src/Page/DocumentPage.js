d4667abe34381daa886e87e1617e20f5816e8a9e
Kim0426 <706shin1728@naver.com>
2023-07-06 20:12:45 +0900

    feat:문서 편집 페이지. 문서 리스트 페이지 생성

---
+++ src/Page/DocumentPage.js
@@ -0,0 +1,52 @@
+import {
+  getDocuments,
+  deleteDocument,
+  addNewDocument,
+} from '../api/Document/index.js';
+import DocumentList from '../Component/Documents/DocumentList.js';
+import { push } from '../route.js';
+
+export default function DocumentPage({ $target }) {
+  const $page = document.createElement('div');
+  $page.classList.add('documentPage');
+
+  this.render = async () => {
+    await getDocuments()
+      .then((documents) => documentLists.setState(documents))
+      .catch((e) => alert(e));
+    $target.appendChild($page);
+  };
+
+  this.render();
+
+  const documentLists = new DocumentList({
+    $target: $page,
+    initalState: [],
+    onDocumentClick: (id) => {
+      push(`/documents/${id}`);
+      this.render();
+    },
+
+    onDocumentAdd: async (id) => {
+      addNewDocument(id)
+        .then(({ id: newDocumentId }) => push(`/documents/${newDocumentId}`))
+        .catch((e) => {
+          alert(e);
+        });
+    },
+
+    onDocumentDelete: async (id) => {
+      deleteDocument(id)
+        .then(({ parent }) => {
+          if (parent.id) {
+            push(`/documents/${parent.id}`);
+          } else {
+            push(`/`);
+          }
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
+  });
+}
