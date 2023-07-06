d3f3b9e5b191d48928804d42679a2f9517016337
Lim_JiSeon <dpf226@naver.com>
2023-07-07 08:45:42 +0900

    [FEAT] 한 화면에 목록과 편집기 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { initRouter } from "./router.js";
 export default function App({ $target }) {
   const postPage = new PostPage({
     $target,
+    initialState: {
+      postId: "new",
+      post: {
+        title: "",
+        content: "",
+      },
+    },
   });
 
   const postEditPage = new PostEditPage({
@@ -23,17 +30,14 @@ export default function App({ $target }) {
     $target.innerHTML = "";
     const { pathname } = window.location;
 
-    if (pathname === "/") {
-      postPage.setState();
-    } else if (pathname.indexOf("/") === 0) {
+    if (pathname !== '/' && pathname.indexOf("/") === 0) {
       const [, postId] = pathname.split("/");
       postEditPage.setState({ postId });
     }
+    postPage.setState()
   };
 
   this.route();
 
   initRouter(() => this.route());
-
-  console.log(request());
 }
