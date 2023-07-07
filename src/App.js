d09b9974af82c455c620d953f3eea36b501ed8b1
jkea1 <jinkyng7890@gmail.com>
2023-07-07 14:51:58 +0900

    주석 정리

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import DocumentsPage from './DocumentsPage.js';
 import ListComponent from './ListComponent.js';
 import { initRouter } from './router.js';
 
-// url 규칙
-// 루트: DocumentList만 그리기
-// ListComponent -> /documents : DocumentList + DocumentList에 새 document 목록생성
-// DocumentsPage -> /docuements/{id} : DocumentList + id에 해당하는 doc 그리기
-
 export default function App({ target }) {
   const listComponent = new ListComponent({ target });
 
