ca4eaebfd865df5575f0e26d8490a4039dd699e6
Kim0426 <706shin1728@naver.com>
2023-07-09 21:06:38 +0900

    style:불필요한 주석 제거

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import { initRouter } from './route.js';
 import DocumentEditPage from './Page/DocumentEditPage.js';
 import DocumentPage from './Page/DocumentPage.js';
 
-// 앱은 라우터를 초기화해주고, 두개의 페이지를 만듬!(문서 리스트페이지와, 문서 편집 페이지)
-
 export default function App({ $target, initalState }) {
   const documentPage = new DocumentPage({
     $target,
     initalState,
   });
 
-  // 문서 id에 따라 다른 문서를 그려줘야함. 초기 id는 new로 잡음
   const documentEditPage = new DocumentEditPage({
     $target,
-    // 초기 상태 지정
     initalState: {
       documentId: 'new',
       document: {
@@ -21,13 +17,11 @@ export default function App({ $target, initalState }) {
         content: '',
       },
     },
-    // 편집기의 제목이 바뀔때, 문서의 리스트들을 다시 그려주는 험수를 props로 내려줌
     onChangeEditorTitle: () => {
       documentPage.render();
     },
   });
 
-  // pathname에 따라 다른 페이지의 상태를 바꾸고 랜더링을 해줌
   this.route = () => {
     const { pathname } = window.location;
 
@@ -46,7 +40,6 @@ export default function App({ $target, initalState }) {
 
   this.route();
 
-  // 라우터 초기화
   initRouter(() => this.route());
 
   window.addEventListener('popstate', () => this.route());
