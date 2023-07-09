4a98a5832c19c94ad03b6fbf963d286e79cbbb45
Kim0426 <706shin1728@naver.com>
2023-07-09 21:00:19 +0900

    refactor:디바운스 시간 상수화

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,5 @@
-const API_ENDPOINT = 'https://kdt-frontend.programmers.co.kr/documents'
-const USER_NAME = 'khj0426'
+const API_ENDPOINT = 'https://kdt-frontend.programmers.co.kr/documents';
+const USER_NAME = 'khj0426';
+const DOCUMENT_DEBOUNCE_TIME = 1500;
 
-export { API_ENDPOINT, USER_NAME }
+export { API_ENDPOINT, USER_NAME, DOCUMENT_DEBOUNCE_TIME };
