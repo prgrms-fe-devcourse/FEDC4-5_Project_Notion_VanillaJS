80421099e98caece016f376232355d38d6acaeff
kim <perfumeplaylist.gmail.com>
2023-07-09 15:19:41 +0900

    feat : 상수/방어코드 구축

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,37 @@
 // api
+// export const API = {
+//   END_POINT: "https://kdt-frontend.programmers.co.kr",
+//   HEADER_NAME: "JungHo-P",
+// };
+
 export const API_END_POINT = "https://kdt-frontend.programmers.co.kr";
 export const API_HEADER_NAME = "JungHo-P";
 
-// storaged
+// storaged keyWord
+// export const KEYWORD = {
+//   VISITED_LOCAL_KEY: "visitedId",
+//   NEW_POST_KEY: "new",
+// };
+
 export const VISITED_LOCAL_KEY = "visitedId";
 export const NEW_POST_KEY = "new";
 
 // event
 export const ROUTER_EVENT_NAME = "route-change";
+
+// initalData
+// export const INITALDATA = {
+//   NEW_POST_DATA: {
+//     id: NEW_POST_KEY,
+//     title: "Untitle",
+//     documents: [],
+//   },
+//   UNTITLE: "Untitle",
+// };
+
+export const NEW_POST_DATA = {
+  id: "new",
+  title: "Untitle",
+  documents: [],
+};
+export const UNTITLE = "Untitle";
