1cadb4179207217810580ee2a0984ad2060a08ef
Kim0426 <706shin1728@naver.com>
2023-07-09 16:28:36 +0900

    refacator:Page변수 네이밍에서 $ 제거, 새 문서 추가시 라우팅 지정

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import { initRouter } from './route.js'
-import DocumentEditPage from './Page/DocumentEditPage.js'
-import DocumentPage from './Page/DocumentPage.js'
+import { initRouter } from './route.js';
+import DocumentEditPage from './Page/DocumentEditPage.js';
+import DocumentPage from './Page/DocumentPage.js';
 
 // 앱은 라우터를 초기화해주고, 두개의 페이지를 만듬!(문서 리스트페이지와, 문서 편집 페이지)
 
-export default function App ({ $target, initalState }) {
-  const $documentPage = new DocumentPage({
+export default function App({ $target, initalState }) {
+  const documentPage = new DocumentPage({
     $target,
-    initalState
-  })
+    initalState,
+  });
 
   // 문서 id에 따라 다른 문서를 그려줘야함. 초기 id는 new로 잡음
-  const $documentEditPage = new DocumentEditPage({
+  const documentEditPage = new DocumentEditPage({
     $target,
     // 초기 상태 지정
     initalState: {
       documentId: 'new',
       document: {
         title: '',
-        content: ''
-      }
+        content: '',
+      },
     },
     // 편집기의 제목이 바뀔때, 문서의 리스트들을 다시 그려주는 험수를 props로 내려줌
     onChangeEditorTitle: () => {
-      $documentPage.render()
-    }
-  })
+      documentPage.render();
+    },
+  });
 
   // pathname에 따라 다른 페이지의 상태를 바꾸고 랜더링을 해줌
   this.route = () => {
-    const { pathname } = window.location
+    const { pathname } = window.location;
 
     if (pathname === '/') {
-      $documentPage.render()
-      $documentEditPage.setState({
-        documentId: 'new'
-      })
+      documentPage.render();
+      documentEditPage.setState({
+        documentId: 'new',
+      });
     } else if (pathname.includes('/documents/')) {
-      const [, , documentId] = pathname.split('/')
+      const [, , documentId] = pathname.split('/');
 
-      $documentPage.render()
-      $documentEditPage.setState({ documentId })
+      documentPage.render();
+      documentEditPage.setState({ documentId });
     }
-  }
+  };
 
-  this.route()
+  this.route();
 
   // 라우터 초기화
-  initRouter(() => this.route())
+  initRouter(() => this.route());
 
-  window.addEventListener('popstate', () => this.route())
+  window.addEventListener('popstate', () => this.route());
 }
