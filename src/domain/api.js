b45e86c79de6e9a86d29f8f9d2ac2da3b12234ed
JiYoon <angella990825@gmail.com>
2023-07-11 11:30:29 +0900

    refactor: 상수 변수 파일 내로 이동

--- src/domain/api.js
+++ src/domain/api.js
@@ -1,4 +1,6 @@
-import { VITE_API_END_POINT, VITE_USERNAME, errorMessages } from '../constants';
+import { VITE_API_END_POINT, VITE_USERNAME } from '../constants';
+const API_FAILURE_MESSAGE = 'API 처리 중 뭔가 이상합니다!';
+const API_ERROR_MESSAGE = '무엇인가 이상합니다!';
 
 // API 요청
 export const request = async (url, options = {}) => {
@@ -13,8 +15,8 @@ export const request = async (url, options = {}) => {
     if (res.ok) {
       return await res.json();
     }
-    throw new Error('api 요청 실패');
+    throw new Error(API_FAILURE_MESSAGE);
   } catch (e) {
-    throw new Error(`${errorMessages.API_ERROR_MESSAGE} ${e.message}`);
+    throw new Error(API_ERROR_MESSAGE);
   }
 };
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,20 +1,22 @@
-import { errorMessages } from '../constants';
+const NEED_NEW_KEYWORD = 'new 키워드를 붙여주세요!';
+const NEED_ARRAY_TYPE = '배열로 입력해야 합니다!';
+const NEED_STRING_TYPE = '제목과 내용은 문자열이어야 합니다!';
 
 export const validateComponent = (target) => {
   if (!target) {
-    throw new Error(errorMessages.NEED_NEW_KEYWORD);
+    throw new Error(NEED_NEW_KEYWORD);
   }
 };
 
 export const validateArray = (array) => {
   if (!Array.isArray(array)) {
-    throw new Error(errorMessages.NEED_ARRAY_TYPE);
+    throw new Error(NEED_ARRAY_TYPE);
   }
 };
 
 export const validateInputData = (data) => {
   const { title, content } = data;
   if (typeof title !== 'string' || typeof content !== 'string') {
-    throw new Error(errorMessages.NEED_STRING_TYPE);
+    throw new Error(NEED_STRING_TYPE);
   }
 };
