33d0686e81fb36fec3cdb5c6160e4b695b39ec21
hyesung99 <4538asd@gmail.com>
2023-07-12 00:45:34 +0900

    refactor : document key 상수로 관리

--- src/service/documentService.js
+++ src/service/documentService.js
@@ -1,8 +1,10 @@
 import { request } from "../api.js";
 import { DocumentTree, Document } from "../domain/index.js";
 
+const DOCUMENT_KEY = "documents";
+
 export const getDocumentTree = async () => {
-  return await request("/documents", {
+  return await request(DOCUMENT_KEY, {
     mothod: "GET",
   }).then((res) => {
     return new DocumentTree({ documentTree: res, isInput: false });
@@ -10,7 +12,7 @@ export const getDocumentTree = async () => {
 };
 
 export const getDocument = async () => {
-  return await request(`/documents/${getDocumentIdByPathname()}`, {
+  return await request(`/${DOCUMENT_KEY}/${getDocumentIdByPathname()}`, {
     mothod: "GET",
   }).then((res) => {
     if (res.content === null) {
