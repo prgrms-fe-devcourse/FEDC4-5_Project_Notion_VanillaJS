f3fa1a39e455793dbd076d9ac0d9c56b506ccb94
hayamaster <dnfxmfkdbzos@naver.com>
2023-07-14 17:41:16 +0900

    (리뷰반영) main.js에서 변수명 변경

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import App from "./App.js";
 
 const $app = document.querySelector("#app");
-const userName = "Haya";
+const username = "Haya";
 
-new App({ $target: $app, username: userName });
+new App({ $target: $app, username });
