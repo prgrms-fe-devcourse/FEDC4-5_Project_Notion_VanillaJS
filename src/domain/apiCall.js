dda9d9c646baeddab3adae96e41451fcb39651b0
JiYoon <angella990825@gmail.com>
2023-07-14 18:55:09 +0900

    refactor: apiCall

--- src/domain/apiCall.js
+++ src/domain/apiCall.js
@@ -1,27 +1,28 @@
 import { request } from './api';
 import { push } from './router';
 
-export const getDocument = async (id) => {
+export { getDocument, createDocument, updateDocument, deleteDocument };
+const getDocument = async (id) => {
   return await request(`/documents/${id}`, {
     method: 'GET',
   });
 };
 
-export const createDocument = async (document) => {
+const createDocument = async (document) => {
   return await request(`/documents`, {
     method: 'POST',
     body: JSON.stringify(document),
   });
 };
 
-export const updateDocument = async (id, document) => {
+const updateDocument = async (id, document) => {
   return await request(`/documents/${id}`, {
     method: 'PUT',
     body: JSON.stringify(document),
   });
 };
 
-export const deleteDocument = async (id) => {
+const deleteDocument = async (id) => {
   await request(`/documents/${id}`, {
     method: 'DELETE',
   });
