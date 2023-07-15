1202f7283cc327a8f0e188249ea84f55ddc8d2a8
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-07-15 23:33:53 +0900

    Refactor: eslint 설정 변경 - no-new는 headerWrapper에서만 비활성화 하므로, 전역에서 필요없는 설정 - no-unresolved는 없는 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,7 @@ module.exports = {
     },
   },
   rules: {
-    'no-new': 0, // new 연산자를 사용하고 할당하지 않아도 경고하지 않는다.
-    'no-unresolved': 0, // import 구문에서 경로를 검사하지 않는다.
-    'prettier/prettier': 'error', // prettier의 문법 검사 결과를 오류로 처리한다.
+    'prettier/prettier': ['error', { endOfLine: 'auto' }], // prettier의 문법 검사 결과를 오류로 처리한다.
     'import/no-unresolved': 2, // import 구문에서 경로를 검사한다.
     'import/no-cycle': 0, // 재귀 참조를 허용한다.
     'no-restricted-globals': 0, // window.alert 같은 내장 전역 변수를 사용할 수 있다.
