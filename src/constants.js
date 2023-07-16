53c8d958d715473a95c3d9de76295997c6c4e309
seeyoujeong <chan3358@naver.com>
2023-07-16 09:53:20 +0900

    chore: gitignore 삭제

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const API_ENDPOINT = "https://kdt-frontend.programmers.co.kr";
+export const API_HEADER_X_USERNAME = "seeyoujeong";
+export const IS_OPEN_STORAGE_KEY = "is_Open_key";
--- src/constantsSample.js
+++
@@ -1,3 +0,0 @@
-export const API_ENDPOINT = "https://example.com";
-export const API_HEADER_X_USERNAME = "apiHeaderXUsername";
-export const IS_OPEN_STORAGE_KEY = "isOpenStorageKey";
