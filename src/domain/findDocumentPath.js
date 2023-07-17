8e9621a20514cb5bf35ff720e0602ad7655d0e76
dlwl98 <ff981113@hanyang.ac.kr>
2023-07-17 14:08:21 +0900

    comment: findDocumentPath 함수 jsDoc 작성

--- src/domain/findDocumentPath.js
+++ src/domain/findDocumentPath.js
@@ -1,3 +1,11 @@
+/**
+ * 특정 document에 도달하기까지 거쳐야 하는 상위 document들의 경로를 반환
+ * @typedef {{id: number; title: string; documents: document[]}} document
+ * @param {document[]} documents
+ * @param {number} documentId
+ * @param {{id: number; title: string}[]} path
+ * @returns
+ */
 export function findDocumentPath(documents, documentId, path = []) {
   for (const document of documents) {
     if (document.id === documentId) {
