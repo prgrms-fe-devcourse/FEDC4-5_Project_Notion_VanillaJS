2c65161d3aca67bdf4c8962cd65242d64a230aca
이민희 <mhl98@dgu.ac.kr>
2023-07-17 17:12:22 +0900

    refactor: debounce 함수화

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import SideBar from "./components/sideBar/SideBar.js";
 import DocumentContent from "./components/content/DocumentContent.js";
 import Default from "./components/content/Default.js";
 import { initRouter } from "./utils/router.js";
+import { debounce } from "./utils/debounce.js";
 import { request } from "./api.js";
 
 export default function App({ $target }) {
-  let timer = null;
   const sideBar = new SideBar({ $target });
   const content = new DocumentContent({
     $target,
     initialState: null,
-    onEditing: (document, id) => {
-      if (timer !== null) {
-        clearTimeout(timer);
+    onEditing: debounce(async (document, id) => {
+      if (id) {
+        await request(`/documents/${id}`, {
+          method: "PUT",
+          body: JSON.stringify(document),
+        });
       }
-      timer = setTimeout(async () => {
-        if (id) {
-          await request(`/documents/${id}`, {
-            method: "PUT",
-            body: JSON.stringify(document),
-          });
-        }
-      }, 2000);
-    },
+    }, 500),
   });
 
   this.route = async () => {
@@ -30,12 +25,12 @@ export default function App({ $target }) {
     const { pathname } = window.location;
     const rootDocuments = await request("/documents");
     if (pathname === "/") {
-      await sideBar.setState(rootDocuments);
+      sideBar.setState(rootDocuments);
       new Default({ $target });
     } else if (pathname.indexOf("/documents/") === 0) {
       const [, , documentId] = pathname.split("/");
       // TODO: 우측 편집기 초기화
-      await sideBar.setState(rootDocuments);
+      sideBar.setState(rootDocuments);
       content.setId(documentId);
     }
   };
