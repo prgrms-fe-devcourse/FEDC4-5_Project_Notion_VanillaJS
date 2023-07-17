7608b7ecc83f65f0a86e9e108cd2761f96ff39f3
JiYoon <angella990825@gmail.com>
2023-07-18 01:03:00 +0900

    rename: 폴더명, 함수명 수정

--- src/utils/validateInputData.test.js
+++ src/utils/validateInputData.test.js
@@ -1,26 +1,26 @@
-import { validateString } from './validation';
+import { validateInputData } from './validation';
 import { errorMessages } from '../constants';
 
 test('문자열이 아니면 오류 발생 o, 테스트 통과', () => {
   expect(() => {
-    validateString({ title: 123, content: 'Hello' });
+    validateInputData({ title: 123, content: 'Hello' });
   }).toThrowError(errorMessages.NEED_STRING_TYPE);
 
   expect(() => {
-    validateString({ title: 'Hello', content: null });
+    validateInputData({ title: 'Hello', content: null });
   }).toThrowError(errorMessages.NEED_STRING_TYPE);
 
   expect(() => {
-    validateString({ title: 'Hello', content: {} });
+    validateInputData({ title: 'Hello', content: {} });
   }).toThrowError(errorMessages.NEED_STRING_TYPE);
 });
 
 test('문자열이면 오류 발생 x, 테스트 통과', () => {
   expect(() => {
-    validateString({ title: 'Hello', content: 'World' });
+    validateInputData({ title: 'Hello', content: 'World' });
   }).not.toThrow();
 
   expect(() => {
-    validateString({ title: '', content: '' });
+    validateInputData({ title: '', content: '' });
   }).not.toThrow();
 });
