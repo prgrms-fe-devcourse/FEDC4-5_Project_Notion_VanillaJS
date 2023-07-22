d7f94cbc06f773dc54a20a272a47237c706ed1e3
JiYoon <angella990825@gmail.com>
2023-07-22 16:40:08 +0900

    refactor: vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 export default {
   build: {
     target: ['edge79', 'firefox68', 'chrome61', 'safari11.1'],
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
 };
