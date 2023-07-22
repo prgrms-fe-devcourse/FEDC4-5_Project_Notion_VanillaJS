4986e09a7e88c261bcc460ab556ca5f6a7bc9855
JiYoon <angella990825@gmail.com>
2023-07-22 17:27:26 +0900

    fix: vite오류

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 export default {
   build: {
     target: ['edge79', 'firefox68', 'chrome61', 'safari11.1'],
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 };
